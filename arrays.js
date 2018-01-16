1d342ea35e1632a188fad274b48c319c3f285030
Carine Tomassian <carinetomassian@gmail.com>
2018-01-16 05:45:20 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,7 +30,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  return array.slice(-1);
+  array.slice(1);
+  return array;
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
